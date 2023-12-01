de0e405ce48b82e5e40f2cbce8711b611efdb222
Cherry <cherry@sparklet.org>
2023-12-01 17:13:18 -0500

    1b finished except for bug...?

--- src/_1b.rs
+++ src/_1b.rs
@@ -1,11 +1,32 @@
 use std::collections::VecDeque;
 
+type NumberKVPair = (&'static str, u32);
+const NUMBERS: [NumberKVPair; 10] = [
+    ("zero", 0),
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
 pub fn run(input: &str) {
     let sum = input.lines().map(parse_line).sum::<u32>();
     println!("{}", sum);
 }
 
-fn mem_is_number(deque: &[char]) -> Option<u32> {
+fn mem_is_number(slice: &[char]) -> Option<u32> {
+    let word = slice.iter().collect::<String>();
+    for (k, v) in NUMBERS {
+        if word.starts_with(k) {
+            return Some(v);
+        }
+    }
+
     None
 }
 
