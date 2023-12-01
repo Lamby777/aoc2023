8d420704428d03fc5dca9c9b04372c7a2b918638
Cherry <cherry@sparklet.org>
2023-12-01 16:28:47 -0500

    finish lv 1

--- src/_1a.rs
+++ src/_1a.rs
@@ -1,3 +1,19 @@
 pub fn run(input: &str) {
-    println!("{}", input);
+    let values = input.lines().map(line_to_calibration);
+    let sum = values.sum::<u32>();
+    println!("{}", sum);
+}
+
+pub fn line_to_calibration(line: &str) -> u32 {
+    let mut chars = line.chars().filter(|v| v.is_digit(10));
+
+    fn char_to_digit(c: char) -> u32 {
+        c.to_digit(10).unwrap()
+    }
+
+    let n_first = chars.next().map(char_to_digit).unwrap();
+    let n_last = chars.next_back().map(char_to_digit);
+    let n_last = n_last.unwrap_or(n_first);
+
+    (n_first * 10) + n_last
 }
