d29656eb9c14ac7189f4e40c0a361c9115657425
Cherry <cherry@sparklet.org>
2023-12-02 01:16:18 -0500

    add unit test

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,45 @@
 
 mod macros;
 
+#[derive(Debug, PartialEq)]
 struct Game {
     id: u32,
 
     picks: Vec<RGB>,
 }
 
+impl Game {
+    fn from_line(line: &str) -> Self {
+        Self {
+            id: todo!(),
+            picks: todo!(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 struct RGB(u32, u32, u32);
 
 fn main() {
     let input = inputfile!("2.txt");
+
+    let lines = input.lines();
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_line_works() {
+        let line = "Game 1: 4 blue; 1 green, 2 red; 4 blue, 1 green, 6 red";
+        let game = Game::from_line(line);
+
+        assert_eq!(
+            game,
+            Game {
+                id: 1,
+                picks: vec![RGB(2, 1, 4), RGB(6, 1, 4)],
+            }
+        )
+    }
 }
