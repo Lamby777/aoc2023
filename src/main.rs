7b293c1c52c2ebed33d0acc53d07000e5f6fd182
Cherry <cherry@sparklet.org>
2023-12-03 03:38:33 -0500

    parse number

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,68 @@ mod macros;
 const WIDTH: usize = 140;
 
 fn is_symbol(ch: char) -> bool {
-    ch.is_digit(10) && ch != '.'
+    !(ch.is_digit(10) || ch == '.')
+}
+
+fn num_prefix(input: &str) -> String {
+    input
+        .chars()
+        .take_while(|v| v.to_digit(10).is_some())
+        .collect::<String>()
 }
 
 fn find_part_numbers(input: Vec<&str>) -> Vec<u32> {
-    let res = vec![];
+    let mut part_numbers = vec![];
 
     // empty line iter used to make start/end follow
     // the same rules as the rest of the lines
     let literally_nothing = ".".repeat(WIDTH);
 
     for i in 0..input.len() {
-        let current = input[i];
+        let line = input[i];
 
-        let line_iter = |i: usize| {
+        let line_factory = |i: usize| {
             input
                 .get(i)
                 .map(|v| v.chars())
                 .unwrap_or(literally_nothing.chars())
+                .map(is_symbol)
+                .collect::<Vec<_>>()
         };
 
-        let previous = line_iter(i - 1);
-        let next = line_iter(i + 1);
+        let previous = line_factory(i - 1);
+        let next = line_factory(i + 1);
 
-        for ch in current.chars() {
+        // the last remembered index of where a
+        // number started
+        let mut is_part = false;
+        let mut num_index = 0;
+
+        for ch in line.chars().chain(std::iter::once('.')) {
+            if !ch.is_digit(10) && is_part {
+                // parse the number
+                let num = num_prefix(&line[num_index..]);
+                let num = num.parse::<u32>().unwrap();
+                part_numbers.push(num);
+            }
+
+            if is_symbol(ch) {
+                is_part = true;
+                continue;
+            }
+
+            if ch == '.' {
+                is_part = false;
+                continue;
+            }
+
             //
         }
+
+        is_part = false;
     }
 
-    res
+    part_numbers
 }
 
 fn main() {
