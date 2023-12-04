74a576ae592c6f26a56115a8bbe965496f765de3
Cherry <cherry@sparklet.org>
2023-12-04 15:48:53 -0500

    :3

--- src/3a.py
+++ src/3a.py
@@ -6,13 +6,23 @@
 #
 
 
+# borrowed, not stolen. :3
+# https://stackoverflow.com/a/5125712/8149876
+class safelist(list):
+    def get(self, index, default=None):
+        try:
+            return self.__getitem__(index)
+        except IndexError:
+            return default
+
+
 # read input file as list of chars
 with open("inputs/3.txt") as f:
     first_line = f.readline()
     LINE_LEN = len(first_line)
 
-    chars = (first_line + f.read()).replace("\n", "")
-    chars = list(chars)
+    _chars = (first_line + f.read()).replace("\n", "")
+    chars = safelist(_chars)
 
 # could probably be a loop but i'm lazy rn :P
 OFFSETS = [
@@ -28,13 +38,19 @@ OFFSETS = [
 ]
 
 
-def is_symbol(ch: str):
-    return (not ch.isdigit()) and (ch != ".")
+def is_symbol(ch: str | None):
+    return ch is not None and (not ch.isdigit()) and (ch != ".")
 
 
 def is_partnum(i: int):
-    return any(is_symbol(chars[i + offset]) for offset in OFFSETS)
+    return any(is_symbol(chars.get(i + offset)) for offset in OFFSETS)
 
 
 for ch in chars:
-    pass
+    number_so_far = ""
+    is_part = False
+
+    if ch.isdigit():
+        pass
+    else:
+        pass
