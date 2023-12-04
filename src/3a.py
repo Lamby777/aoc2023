638402220fd993eef289c152cce84a2ed37102d1
Cherry <cherry@sparklet.org>
2023-12-04 16:57:28 -0500

    pre redoing flow control

--- src/3a.py
+++ src/3a.py
@@ -11,8 +11,12 @@ with open("inputs/3.txt") as f:
     lines = f.readlines()
 
 
+def is_blank(ch: str):
+    return ch not in [".", "\n"]
+
+
 def is_symbol(ch: str | None):
-    return ch is not None and (not ch.isdigit()) and (ch != ".")
+    return ch is not None and (not ch.isdigit()) and not is_blank(ch)
 
 
 def getch_safe(line: int, col: int):
@@ -37,6 +41,9 @@ for lineno, line in enumerate(lines):
     number_so_far = ""
     is_part = False
 
+    # lineno = 1
+    # line = lines[1]
+
     for i, ch in enumerate(line):
         vert_scan = vertical_symbol_scan(lineno, i)
         is_part = is_part or vert_scan
@@ -56,11 +63,16 @@ for lineno, line in enumerate(lines):
 
             # if the char after the number is a symbol,
             # then the number is still a part number
-            if is_part or ch != ".":
+            if is_part or is_symbol(ch):
                 partnums.append(int(number_so_far))
 
             number_so_far = ""
 
+        # TODO handle newlines
+    # break
+
+#
+print(partnums)
 
 # 546184 is "too high" :(
 print(sum(partnums))
