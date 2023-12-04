2ef92f90e94ee5b2e99acc2e24a6fc45c7edad3e
Cherry <cherry@sparklet.org>
2023-12-04 15:31:57 -0500

    read flat list of chars instead of nesting

--- src/3a.py
+++ src/3a.py
@@ -5,18 +5,14 @@
 #  - &Cherry <3
 #
 
-# read input file from inputs/3.txt
+# read input file as list of chars
 with open("inputs/3.txt") as f:
-    lines = f.readlines()
-
-# map each line to a list of chars
-char_grid = [list(line) for line in lines]
+    chars = list(f.read())
 
 
 def is_symbol(ch: str):
     return (not ch.isdigit()) and (ch != ".")
 
 
-for line in char_grid:
-    for ch in line:
-        pass
+for ch in chars:
+    pass
